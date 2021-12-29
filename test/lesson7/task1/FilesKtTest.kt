33e9ae797b4579ba2f1520d8e9a4e5099a663882
Boykova <bojkova.uv@edu.spbstu.ru>
2021-12-30 01:11:54 +0300

    РОБОТ

--- test/lesson7/task1/FilesKtTest.kt
+++ test/lesson7/task1/FilesKtTest.kt
@@ -9,66 +9,6 @@ import org.junit.jupiter.api.Assertions.*
 internal class FilesKtTest {
 
 
-    @Test
-    fun myFunw() {
-        Assertions.assertThrows(IllegalArgumentException::class.java) { myFunw(listOf("Ма Швор"), listOf("")) }
-        assertEquals(
-            listOf("Мария Иванова"), myFunw(
-                listOf(
-                    "Мария Иванова - Фикика 5, Химия 4, Английский 3",
-                    "Данил Иванов - Фикика 3, Химия 4, Английский 5"
-                ), listOf("Английский", "Музыка")
-            )
-        )
-        assertEquals(
-            listOf("Данил Иванов"), myFunw(
-                listOf(
-                    "Мария Иванова - Фикика 5, Химия 4, Английский 3, Музыка 3",
-                    "Данил Иванов - Фикика 4, Химия 4, Английский 5"
-                ), listOf("Английский", "Музыка")
-            )
-        )
-    }
-
-    @Test
-    fun shashki() {
-        assertEquals(
-            "eeeeeeee" + "\n"
-                    + "eeeeeeee" + "\n"
-                    + "eeeeeeee" + "\n"
-                    + "eeeeeeee" + "\n"
-                    + "eeeeeBee" + "\n"
-                    + "eeeeeeee" + "\n"
-                    + "eeeeeeee" + "\n"
-                    + "eeeeeeee",
-
-            shashki(
-                "eeeeeeee" + "\n"
-                        + "eeeeeeee" + "\n"
-                        + "eeeeeeeB" + "\n"
-                        + "eeeeeeWe" + "\n"
-                        + "eeeeeeee" + "\n"
-                        + "eeeeeeee" + "\n"
-                        + "eeeeeeee" + "\n"
-                        + "eeeeeeee", "G6-D3"
-            )
-        )
-        assertThrows(
-            IllegalArgumentException::
-            class.java
-        ) {
-            shashki(
-                "eeeeeeee" + "\n"
-                        + "eeeeeeee" + "\n"
-                        + "eeeeeeWe" + "\n"
-                        + "eeeeeBee" + "\n"
-                        + "eeeeBeee" + "\n"
-                        + "eeeeeeee" + "\n"
-                        + "eeeeeeee" + "\n"
-                        + "eeeeeeee", "G6-D3"
-            )
-        }
-    }
 
     @Test
     fun robot() {
